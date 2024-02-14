5ae3d26e17c0f9942343559b82a18bca9c42c6bd
ahmadxgani <ahmadxgani01@gmail.com>
2024-02-14 21:31:25 +0700

    refactor

--- app/src/main/java/com/dicoding/usersearch/FavoriteListActivity.kt
+++ app/src/main/java/com/dicoding/usersearch/FavoriteListActivity.kt
@@ -3,9 +3,9 @@ package com.dicoding.usersearch
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class SavedListActivity : AppCompatActivity() {
+class FavoriteListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_saved_list)
+        setContentView(R.layout.activity_favorite_list)
     }
 }
